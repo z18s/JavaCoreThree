4c1e3e5066952d67b9a73758823fd2aa437f9625
Konstantin <57114608+z18s@users.noreply.github.com>
2020-05-30 14:02:38 +0300

    Homework Eight additional

---
+++ src/lesson_8/HomeworkEight.java
@@ -0,0 +1,10 @@
+package lesson_8;
+
+public class HomeworkEight {
+
+    public static void main(String[] args) {
+
+        HelixArray helixArray = new HelixArray(5);
+        helixArray.print();
+    }
+}
